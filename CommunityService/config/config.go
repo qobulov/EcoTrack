5e293cfc09b7f6eef61c59d9b9c29be6de209d5c
Fedy1507 <gayratovfirdavs720@gmail.com>
2024-07-05 17:50:18 +0500

    in progres

--- CommunityService/config/config.go
+++ CommunityService/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"os"
+
 	"github.com/joho/godotenv"
 	"github.com/spf13/cast"
 )
@@ -23,11 +24,11 @@ func Load() Config {
 
 	config := Config{}
 	config.DB_HOST = cast.ToString(Coalesce("DB_HOST", "localhost"))
-	config.DB_NAME = cast.ToString(Coalesce("DB_NAME", "userservice"))
+	config.DB_NAME = cast.ToString(Coalesce("DB_NAME", "postgres"))
 	config.DB_PORT = cast.ToInt(Coalesce("DB_PORT", 5432))
 	config.DB_USER = cast.ToString(Coalesce("DB_USER", "postgres"))
-	config.DB_PASSWORD = cast.ToString(Coalesce("DB_PASSWORD", "hamidjon4424"))
-	config.URL_PORT = cast.ToString(Coalesce("URL_PORT", "50051"))
+	config.DB_PASSWORD = cast.ToString(Coalesce("DB_PASSWORD", "postgres"))
+	config.URL_PORT = cast.ToString(Coalesce("URL_PORT", ":8093"))
 
 	return config
 }
